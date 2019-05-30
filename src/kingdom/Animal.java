89da007e4e491fd5240f3a64d6449521907e32f3
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-05-30 16:01:35 -0400

    Add abstract animal class

---
+++ src/kingdom/Animal.java
@@ -0,0 +1,30 @@
+package kingdom;
+
+public abstract class Animal {
+	private static int maxId = 0;
+	public int id;
+	private String name;
+	private int yearDiscovered;
+
+	public Animal(String name, int yearDiscovered) {
+		id = maxId++;
+		this.name = name;
+		this.yearDiscovered = yearDiscovered;
+	}
+
+	public String eat() {
+		return "Eats";
+	}
+
+	public String move() {
+		return "Moves";
+	}
+
+	public String breathe() {
+		return "Breathes";
+	}
+
+	public String reproduce() {
+		return "Reproduces";
+	}
+}
